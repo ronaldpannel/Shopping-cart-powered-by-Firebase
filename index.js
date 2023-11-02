4a87e0e86ec6fd99c78b53ebe19a557eca75bde4
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2023-11-02 10:17:20 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://realtime-database-fc2c3-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
+
+addButtonEl.addEventListener("click", () => {
+  let inputValue = inputFieldEl.value;
+  push(shoppingListInDB, inputValue);
+
+  clearInputFieldEl();
+});
+
+onValue(shoppingListInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
+    clearShoppingListEl();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
+      appendToList(currentItem);
+    }
+  } else {
+    shoppingListEl.innerHTML = "no Items added yet";
+  }
+});
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = " ";
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = " ";
+}
+function appendToList(item) {
+  // shoppingListEl.innerHTML += `<li> ${itemValue}</li>`;
+  let itemID = item[0];
+  let itemValue = item[1];
+
+  let newEl = document.createElement("li");
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("click", () => {
+    let exactLocationOfItemInDatabase = ref(database, `shoppingList/${itemID}`);
+    remove(exactLocationOfItemInDatabase);
+  });
+
+  shoppingListEl.append(newEl);
+}
